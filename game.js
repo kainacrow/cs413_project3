548de613e6dea7fb9f1677b9e1207349ecca88c8
Kaina <kaina.crow@gmail.com>
2016-05-27 15:10:34 -0700

    collision is still wonky but it works in certain spots

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var gameHeight = 3560;
-var gameWidth = 3560;
+var gameHeight = 560;
+var gameWidth = 560;
 var gameScale = 4;
 var DIM = 12;
 // var horizon_y = gameHeight/gameScale/2;
@@ -9,6 +9,15 @@ var gameport = document.getElementById("gameport");
 var renderer = PIXI.autoDetectRenderer(gameWidth, gameHeight, {backgroundColor: 0x3bd2f2});
 gameport.appendChild(renderer.view);
 
+// var start = new PIXI.Sprite(PIXI.Texture.fromImage("instructions1.png"));
+var startText = new PIXI.Text("Press Enter\nto play", {font: "12px Monospace", fill: "#fff", strokeThickness: 3});
+startText.position.y = 50;
+startText.position.x = 20;
+
+var winText = new PIXI.Text("You have\nsuccessfully\nkilled all\nthe villains\nCongrats!", {font: "12px Monospace", fill: "#fff", strokeThickness: 3});
+winText.position.y = 20;
+winText.position.x = 20;
+
 var stage = new PIXI.Container();
 stage.scale.x = gameScale;
 stage.scale.y = gameScale;
@@ -17,6 +26,7 @@ var look_left = false;
 var look_right = true;
 
 var laser = new PIXI.Sprite(PIXI.Texture.fromImage('laser.png'));
+var start = false;
 
 // Scene objects get loaded in the ready function
 var player;
@@ -29,6 +39,13 @@ var moveRight = 2;
 var moveUp = 3;
 var moveDown = 4;
 var moveNone = 0;
+
+
+var enemy;
+var enemies = [];
+
+var game = false;
+var speed = 30;
 
 // the move function starts of continues movement
 function move() {
@@ -47,18 +64,18 @@ function move() {
   if (player.direction == moveLeft) {
     look_left = true;
     look_right = false;
-    createjs.Tween.get(player).to({x: player.x - 10}, 500).call(move);
+    createjs.Tween.get(player).to({x: player.x - speed}, 500).call(move);
   }
   if (player.direction == moveRight) {
       look_right = true;
       look_left = false;
-      createjs.Tween.get(player).to({x: player.x + 10}, 500).call(move);
+      createjs.Tween.get(player).to({x: player.x + speed}, 500).call(move);
   }
   if (player.direction == moveUp)
-    createjs.Tween.get(player).to({y: player.y - 10}, 500).call(move);
+    createjs.Tween.get(player).to({y: player.y - speed}, 500).call(move);
   
   if (player.direction == moveDown)
-    createjs.Tween.get(player).to({y: player.y + 10}, 500).call(move);
+    createjs.Tween.get(player).to({y: player.y + speed}, 500).call(move);
     
   if(look_left){
       player.scale.x = -1;
@@ -95,6 +112,11 @@ window.addEventListener("keydown", function (e) {
   
   player.direction = moveNone;
 
+  if (e.keyCode == 13){
+    start.alpha = 0;
+    startText.alpha = 0;
+    game = true;
+}
   if (e.keyCode == 87)
     player.direction = moveUp;
   else if (e.keyCode == 83)
@@ -107,6 +129,7 @@ window.addEventListener("keydown", function (e) {
       laser.visible = true;
       shoot();
   }
+  
 
 //   console.log(e.keyCode);
   move();
@@ -128,9 +151,6 @@ PIXI.loader
     .add('man', 'player.png')
     .add('spritesheet', 'spritesheet.json')
     .load(ready);
-
-var enemy;
-var enemies = [];
     
 // ready function
 function ready() {
@@ -141,7 +161,7 @@ function ready() {
         frames.push(PIXI.Texture.fromImage('man' + i + '.png'))
     }
    
-    createjs.Ticker.setFPS(60);
+    //createjs.Ticker.setFPS(60);
     var tu = new TileUtilities(PIXI);
     world = tu.makeTiledWorld("map_json", "map.png");
     stage.addChild(world);
@@ -163,23 +183,33 @@ function ready() {
         
     laser.visible = false;
     entity_layer.addChild(laser);
+
     
     for (i = 0 ; i < 5; i ++){
         enemies[i] =  new PIXI.extras.MovieClip(frames);
         enemies[i].animationSpeed = 0.1;
-        enemies[i].position.y = Math.floor(Math.random() * 550) + 50;
-        enemies[i].position.x = Math.floor(Math.random() * 550) + 50;
+        enemies[i].position.y = Math.floor(Math.random() * 500) + 50;
+        enemies[i].position.x = Math.floor(Math.random() * 500) + 50;
         enemies[i].anchor.x = .5;
         enemies[i].anchor.y = .5;
         entity_layer.addChild(enemies[i]);
         enemies[i].play();
     }
     
+    laser.x = player.position.x;
+    laser.y = player.position.y;
+    laser.visible = false;
+    entity_layer.addChild(laser);
+    left();  
+    entity_layer.addChild(startText);
+    entity_layer.addChild(winText);
+    winText.visible = false;
+    
     //water = world.getObject("Water").data;
     
-    player.direction = moveNone;
-    player.moving = false;
-    animate();
+    // player.direction = moveNone;
+    // player.moving = false;
+    // animate();
     }
 
 
@@ -190,43 +220,68 @@ function box_point_intersection(box, x, y) {
   if (y > box.position.y + box.height) return false;
   return true;
 }
-
-// function left() {
-//     for (i = 0 ; i < 7; i ++){
-//     createjs.Tween.get(enemies[i].position).to({x: enemies[i].position.x +20}, 4000, createjs.Ease.cubicOut);
+// previousX = enemies[i].position.x;
+function left() {
+    for (i = 0 ; i < 5; i ++){
+    createjs.Tween.get(enemies[i].position).to({x: enemies[i].position.x +50}, 4000, createjs.Ease.cubicOut);
+    }
+}
+// function right() {
+//     for (i = 0; i < 5; i++) {
+//         createjs.Tween.get(enemies[i].position).to({x: previousX -50}, 4000, create.js.Ease.cubicOut);
 //     }
 // }
+
 
 
 function shoot() {
     if (laser.visible == true){
         laser.x = player.position.x;
-        laser.y = player.position.y+player.height/2;
+        laser.y = player.position.y;
         createjs.Tween.removeTweens(laser.position);
         if(look_right){
-            createjs.Tween.get(laser.position).to({x:laser.position.x +50}, 300);
+            createjs.Tween.get(laser.position).to({x:laser.position.x +100}, 700);
         }
         if(look_left){
-            createjs.Tween.get(laser.position).to({x:laser.position.x -50}, 300);
+            createjs.Tween.get(laser.position).to({x:laser.position.x -100}, 700);
         }
     }
 }
+animate();
 
 // animate function
-function animate() {
+function animate(timestamp) {
     requestAnimationFrame(animate);
     update_camera();
     renderer.render(stage);
     for (i = 0 ; i < 5; i ++){
         if (box_point_intersection(enemies[i], laser.x, laser.y)) {
-        createjs.Tween.removeTweens(laser.position);
-        createjs.Tween.removeTweens(enemies[i].position);
-        createjs.Tween.get([enemies]).to({alpha: 0}, 500);
-        laser.visible = false;    
-}
+            //alert("hit");
+            createjs.Tween.removeTweens(laser.position);
+            //createjs.Tween.removeTweens(enemies[i].position);
+            createjs.Tween.get(enemies[i]).to({alpha: 0}, 500);
+            laser.visible = false;    
+        }
     }
 }
-animate();
+
+function win() {
+    var score = 0;
+    for (i=0; i<5; i++){
+        if (enemies[i].alpha == 0){
+            score +=1;
+        }
+    }
+    if (score == 5) {
+        winText.position.x = player.position.x;
+        winText.position.y = player.position.y;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 
 // camera movement
 function update_camera() {
